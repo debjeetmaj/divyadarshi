75969463f4f32526d6ab741d650dd7e9cf6809d6
Debjeet Majumdar <debjeetmaj17@gmail.com>
2017-07-30 00:30:56 +0530

    Changing default from Devlopment to Release UI behaviour

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function createWindow () {
     }))
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    //mainWindow.webContents.openDevTools()
 
     mainWindow.on('closed', function(){
         // Dereference the window object, usually you would store windows
